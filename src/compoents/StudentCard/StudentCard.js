868dd92b9b2494b37c17f09772864f244895484e
hector918 <hector_zhong@outlook.com>
2023-07-16 14:51:26 -0400

    styling student list and card

--- src/compoents/StudentCard/StudentCard.js
+++ src/compoents/StudentCard/StudentCard.js
@@ -5,14 +5,18 @@ export default function StudentCard ({student}){
   for(let x of grades) total += Number(x);
   const average = (total / grades.length).toFixed(2);
   console.log(id)
-  return <div>
-    <img src={pic} alt={`${firstName} ${lastName}`} />
-    <h1>{firstName} {lastName}</h1>
-    <ul>
-      <li>Email: {email}</li>
-      <li>Company: {company}</li>
-      <li>Skill: {skill}</li>
-      <li>Average: {average}%</li>
-    </ul>
+  return <div className='StudentCard' key={`studentCard-id-${id}`}>
+    <div className='StudentCard__avatar'>
+      <img src={pic} alt={`${firstName} ${lastName}`} />
+    </div>
+    <div className='StudentCard__info'>
+      <h1>{firstName} {lastName}</h1>
+      <ul>
+        <li>Email: {email}</li>
+        <li>Company: {company}</li>
+        <li>Skill: {skill}</li>
+        <li>Average: {average}%</li>
+      </ul>
+    </div>
   </div>
 }
